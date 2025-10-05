e343ae9aa16b968bc8027b10c8d8212b9d43b058
choido <choido@eseict.com>
2025-10-05 23:20:40 +0900

    feat(main):
    - 마커 프리뷰 이미지 추가

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,17 +1,16 @@
-export const isDev: boolean = process.env.NODE_ENV === "development";
-
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
-const port: number = import.meta.env.VITE_MAP_SERVER_PORT;
-const ctx: string = isDev ? "/map" : "";
+const ctxPath: string = import.meta.env.VITE_MAP_SERVER_CONTEXT_PATH;
 
 export const URL = {
 
     MAP: {
         // LIST: `http://${isDev ? window.location.hostname : "spring-backend"}:${port}${ctx}/api/hist/items`,
         // YEAR: `http://${isDev ? window.location.hostname : "spring-backend"}:${port}${ctx}/api/hist/year-items`
-        LIST: `${baseUrl}/api/hist/items`,
-        YEAR: `${baseUrl}/api/hist/year-items`
-    }
+        LIST: `${baseUrl}/${ctxPath}/api/hist/items`,
+        YEAR: `${baseUrl}/${ctxPath}/api/hist/year-items`
+    },
+    IMAGE: (id: string, fileNm: string) => `${baseUrl}/${ctxPath}/images/`.concat(id.concat("|").concat(fileNm)),
+
 }
 
 export default URL
