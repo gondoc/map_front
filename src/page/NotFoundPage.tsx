f129de881d17696c8f64ebe18245aa4983287d09
choido <choido@eseict.com>
2025-10-05 23:09:29 +0900

    feat(main):
    - 404 page

---
+++ src/page/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+const NotFoundPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/", {replace: true});
+    }, [navigate]);
+
+    return null;
+};
+
+export default NotFoundPage;
