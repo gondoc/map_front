eab3b51b091c8b1a48661d57b9775e0a391c84e2
choido <choido@eseict.com>
2025-10-06 01:05:05 +0900

    feat(main):
    - contact area 추가

--- src/components/common/nav/NavArea.tsx
+++ src/components/common/nav/NavArea.tsx
@@ -3,7 +3,18 @@ import ProjectNavItem from "@component/common/nav/navItem/projects/ProjectNavIte
 import YearNavItem from "@component/common/nav/navItem/year/YearNavItem";
 import TimeLineNavItem from "@component/common/nav/navItem/timeLine/TimeLineNavItem";
 
+import icon_github from "@image/icon_github.png";
+import icon_mail from "@image/icon_mail.png";
+
 const NavArea = () => {
+
+    const githubBtnClickHandler = () => {
+        window.open("https://github.com/gondoc/map_front");
+    }
+
+    const mailBtnClickHandler = () => {
+        window.open("mailto:chlehddh8062@gmail.com");
+    }
 
     return (
         <NavAreaWrapper>
@@ -12,6 +23,14 @@ const NavArea = () => {
                 <YearNavItem title={"year"}/>
                 <TimeLineNavItem title={"timeline"}/>
             </StNavItemArea>
+            <ContactArea>
+                <IconArea $image={icon_github} onClick={() => githubBtnClickHandler()}>
+                    <i></i>
+                </IconArea>
+                <IconArea $image={icon_mail} onClick={() => mailBtnClickHandler()}>
+                    <i></i>
+                </IconArea>
+            </ContactArea>
         </NavAreaWrapper>
     )
 }
@@ -19,10 +38,15 @@ const NavArea = () => {
 export default NavArea
 
 const NavAreaWrapper = styled.nav`
+    position: absolute;
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 100%;
+    justify-content: space-between;
+    height: calc(100vh - 70px);
+    top: 70px;
+    //width: 260px;
+    width: 13.542vw;
     left: 0;
     z-index: 1;
     background: #F7FBFC;
@@ -33,26 +57,46 @@ const StNavItemArea = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    width: 260px;
+    //width: 260px;
+    width: 13.542vw;
     height: auto;
 `
 
 const ContactArea = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-evenly;
     width: 100%;
-    height: 200px;
-
-    background-color: #769FCD;
+    height: 135px;
     row-gap: 13px;
 `
 
-const IconArea = styled.div`
-    width: 30px;
-    height: 30px;
+const IconArea = styled.div<{ $image: string }>`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #4C5580;
+    //width: 85px;
+    width: 4.427vw;
+    //height: 85px;
+    height: 4.427vw;
+    border-radius: 15px;
+    border: 3px solid #769FCD;
+    background-color: #D6E6F2;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #B9D7EA;
+    }
+
+    i {
+        //width: 60px;
+        width: 3.125vw;
+        //height: 60px;
+        height: 3.125vw;
+        background: url(${({$image}) => $image}) no-repeat center/100%;
+        opacity: 0.6;
+    }
+
+    transition: all 250ms;
 `
+// background-image: ${({$image}) => `url(${$image}) no-repeat center/100%`};
